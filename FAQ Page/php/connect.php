3b7c48cd89e8808aca8c19f976c7333c91dd9957
KickeGoodell <112164152+KickeGoodell@users.noreply.github.com>
2023-06-13 09:04:05 +0200

    change database

--- FAQ Page/php/connect.php
+++ FAQ Page/php/connect.php
@@ -4,7 +4,7 @@
 $IP = "10.2.2.195";                                                          // Creates a variable with the IP address to the server
 $username = "enrique";                                                       // Creates a variable with the username to the mysql user I am connecting to 
 $password = "d9g[4k/Pphr10w00";                                              // Creates a variable with the password to the mysq user I am connecting to
-$database = "Arsoppgave";                                                    // Creates a variable with the name of the database I am connecting to
+$database = "HomeProject";                                                    // Creates a variable with the name of the database I am connecting to
 
 $conn = mysqli_connect($IP, $username, $password, $database);                // Creates a variable with the mysqli_connect function by using the variables I created above
 
